2b721a500e83c17bfe8c718e979843469d7f7e41
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-02-21 00:52:05 +0100

    CHECKOUT POST

--- 03-front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -132,7 +132,7 @@ export class CheckoutComponent implements OnInit{
 
   get billingAddressStreet(){return this.checkoutFormGroup.get('billingAddress.street')};
   get billingAddressCity(){return this.checkoutFormGroup.get('billingAddress.city')};
-  get billingAddressState(){return this.checkoutFormGroup.get('shippingAddress.state')};
+  get billingAddressState(){return this.checkoutFormGroup.get('billingAddress.state')};
   get billingAddressZipCode(){return this.checkoutFormGroup.get('billingAddress.zipCode')};
   get billingAddressCountry(){return this.checkoutFormGroup.get('billingAddress.country')};
   get creditCardType(){return this.checkoutFormGroup.get('creditCard.cardType')};
@@ -160,11 +160,12 @@ export class CheckoutComponent implements OnInit{
   }
   onSubmit(){
     console.log("Handling the submit button");
+
     if(this.checkoutFormGroup.invalid){
         this.checkoutFormGroup.markAllAsTouched();
         return;
     }
-    
+
     //set up order 
     let order = new Order();
     order.totalPrice = this.totalPrice;
@@ -192,17 +193,21 @@ export class CheckoutComponent implements OnInit{
     //populate purchase - shipping address
     purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
     const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+    console.log(shippingState);
+
     const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
     purchase.shippingAddress.state = shippingState.name;
     purchase.shippingAddress.country= shippingCountry.name;
 
+
     //populate purchase - billing address
-    purchase.shippingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
     const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+
     const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
     purchase.billingAddress.state = billingState.name;
     purchase.billingAddress.country= billingCountry.name;
-
+    
     //populate purchase - order and order items
     purchase.order = order;
     purchase.orderItems = orderItems;
@@ -212,6 +217,7 @@ export class CheckoutComponent implements OnInit{
     this.checkoutService.placeOrder(purchase).subscribe(
       {
         //success path
+        
         next:  response =>{
           alert(`Your order has been received .\nOrder tracking number: ${response.orderTrackingNumber}`);
           //reset cart 
