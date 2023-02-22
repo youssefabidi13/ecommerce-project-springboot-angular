ca918b11645ed8c77ee1856e6872555c79dfb649
youssefabidi13 <youssef.abidi@etu.uae.ac.ma>
2023-02-22 11:01:04 +0100

    algorithme luhn validator

--- 03-front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-front-end/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -244,7 +244,7 @@ export class CheckoutComponent implements OnInit{
     this.checkoutFormGroup.reset();
 
     //navigate back to the products page 
-    this.router.navigateByUrl("/products")
+    this.router.navigateByUrl("/products");
   }
 
   handleMonthsAndYears(){
